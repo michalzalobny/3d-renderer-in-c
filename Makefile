dc80981471f29f9ce2c802a82aa257e52c7b7f22
Michal Zalobny <michal.zalobny@gmail.com>
2023-11-21 12:40:40 +0000

    Fix SDL2 linking (Both for Makefile and for the vscode extenstion)

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	gcc -Wall -std=c99 ./src/*.c -o renderer
+	gcc -Wall -std=c99 ./src/*.c -o renderer -I/opt/homebrew/include -L/opt/homebrew/lib -lSDL2 
 
 run:
 	./renderer
--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+
+void initialize_window(void){
+  SDL_Init(SDL_INIT_EVERYTHING);
+}
 
 int main (void) {
-  printf("Hello world\n");
+  
+  initialize_window();
+
   return 0;
 }
