1c1b661b2665fc2c4711611575542d7f0c392e10
Michal Zalobny <michal.zalobny@gmail.com>
2023-11-21 15:58:00 +0000

    Add init SDL window and renderer

--- src/main.c
+++ src/main.c
@@ -1,13 +1,46 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
-void initialize_window(void){
-  SDL_Init(SDL_INIT_EVERYTHING);
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool initialize_window(void) {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    fprintf(stderr, "Error initializing SDL.\n");
+    return false;
+  }
+
+  // Create SDL window
+  window = SDL_CreateWindow(
+    NULL, // Window title
+    SDL_WINDOWPOS_CENTERED, // x position
+    SDL_WINDOWPOS_CENTERED, // y position 
+    800, // Window width
+    600, // Window height
+    SDL_WINDOW_BORDERLESS
+  );
+
+  if(!window){
+    fprintf(stderr, "Error creating SDL window.\n");
+    return false;
+  }
+
+  //Create an SDL renderer
+  renderer = SDL_CreateRenderer(window, -1, 0);
+  
+  if(!renderer){
+    fprintf(stderr, "Error creating SDL renderer.\n");
+    return false;
+  }
+
+  return true;
 }
 
 int main (void) {
   
-  initialize_window();
+  is_running = initialize_window();
 
   return 0;
 }
