d23e997a661d83feb05326b4e7178051e5ddb4a6
Michal Zalobny <michal.zalobny@gmail.com>
2023-11-25 11:30:47 +0000

    Add initial game loop

--- src/main.c
+++ src/main.c
@@ -38,9 +38,44 @@ bool initialize_window(void) {
   return true;
 }
 
+void setup(void){}
+
+void process_input(void){
+  SDL_Event event;
+  SDL_PollEvent(&event);
+
+  switch(event.type){
+    case SDL_QUIT:
+      is_running = false;
+      break;
+    case SDL_KEYDOWN:
+      if (event.key.keysym.sym == SDLK_ESCAPE)
+        is_running = false;
+        break;
+  }
+}
+
+void update(void){}
+
+void render(void){
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_RenderClear(renderer);
+
+  SDL_RenderPresent(renderer);
+}
+
+
+
 int main (void) {
-  
   is_running = initialize_window();
+
+  setup();
+
+  while (is_running){
+    process_input();
+    update();
+    render();
+  }
 
   return 0;
 }
