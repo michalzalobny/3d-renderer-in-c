12ec075b9c172b6fdd573031e290ed68248b637a
Michal Zalobny <michal.zalobny@gmail.com>
2023-12-06 17:06:01 +0000

    Update example

--- playground/src/main.c
+++ playground/src/main.c
@@ -9,15 +9,16 @@ int main (void) {
   printf("Size of int: %lu\n", sizeof(int)); // %lu = unsigned long int
   printf("Size of int*: %lu\n", sizeof(int*));
 
-  char my_arr[5] = {'c','d','v','g','k'};
-  char* arr_pointer = my_arr;
+  int my_arr[5] = {32, 55, 12, 34, 6};
+  int* arr_pointer = my_arr; // my_arr == &my_arr[0]
 
   printf("Arr pointer: %p\n", arr_pointer);
-  printf("Value of arr pointer: %c\n", *arr_pointer);
+  printf("Value of arr pointer: %d\n", *arr_pointer); // dereference the pointer
 
-  char* arr_pointer_incremented = (char*)arr_pointer + 2;
+  int* arr_pointer_incremented = (int*)arr_pointer + 1; // moves the pointer to the next int
   printf("Arr pointer incremented: %p\n", arr_pointer_incremented);
-  printf("Value of arr pointer incremented: %c\n", *arr_pointer_incremented);
+  printf("Value of arr pointer incremented: %d\n", *arr_pointer_incremented);
+
 
   printf("--------------------------------------------------\n");
   return 0;
