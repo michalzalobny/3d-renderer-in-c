c68364984e7f4ee2da6af321ac62188f26bef4b6
Michal Zalobny <michal.zalobny@gmail.com>
2023-12-06 17:28:47 +0000

    Implement dynamic array support (WIP)

--- src/main.c
+++ src/main.c
@@ -7,8 +7,10 @@
 #include "display.h"
 #include "vector.h"
 #include "mesh.h"
+#include "array.h"
 
-triangle_t triangles_to_render[N_MESH_FACES];
+// Array of triangles that should be rendere frame by frame
+triangle_t* triangles_to_render = NULL;
 
 vec3_t camera_position = {.x = 0, .y = 0, .z = -5};
 vec3_t cube_rotation = {.x = 0, .y = 0, .z = 0};
@@ -31,10 +33,6 @@ void setup(void){
     window_width,
     window_height
   );
-
- 
-
- 
 }
 
 void process_input(void){
@@ -86,6 +84,9 @@ void update(void) {
   }
   previous_frame_time = SDL_GetTicks();
 
+  // Initialize the array of triangles to render
+  triangles_to_render = NULL;
+
   // Rotate the cube
   cube_rotation.x += 0.01;
   cube_rotation.y += 0.01;
@@ -124,18 +125,19 @@ void update(void) {
     }
 
     // Save the projected triangle in the array of triangles to render
-    triangles_to_render[i] = projected_triangle;
+    // triangles_to_render[i] = projected_triangle;
+    array_push(triangles_to_render, projected_triangle);
   }
-
-
-
 }
 
 void render(void){
   // draw_grid(0xFF0000FF, 15);
 
   // Loop all projected triangles and render them
-  for (int i = 0; i< N_MESH_FACES; i++){
+
+  int num_triangles = array_length(triangles_to_render);
+
+  for (int i = 0; i< num_triangles; i++){
     triangle_t triangle = triangles_to_render[i];
 
     //Draw vertex points
@@ -151,6 +153,9 @@ void render(void){
       0xFF00FF00
     );
   }
+
+  // Clear the array of triangles to render each frame
+  array_free(triangles_to_render);
 
   render_color_buffer();
   clear_color_buffer(0xFF000000);
