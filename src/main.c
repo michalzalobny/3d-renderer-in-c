d61aae2f5d4f103826feb016936f37dcef450de6
Michal Zalobny <michal.zalobny@gmail.com>
2024-01-12 22:04:11 +0000

    Load texture from file

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <SDL2/SDL.h>
 #include <math.h>
 
+#include "upng.h"
 #include "display.h"
 #include "vector.h"
 #include "mesh.h"
@@ -49,14 +50,12 @@ void setup(void){
   float zfar = 10.0;
   proj_matrix = mat4_make_projection(fov, aspect, znear, zfar);
 
-  // Manually load the hardcoded texture data from the static array
-  mesh_texture = (uint32_t*)REDBRICK_TEXTURE;
-  texture_width = 64;
-  texture_height = 64;
-
   // Load the cube values in the mesh data structure
   load_cube_mesh_data();
   //load_obj_file_data("./assets/f22.obj");
+
+  // Load the texture information from an external PNG file
+  load_png_texture_data("./assets/cube.png");
 }
 
 void handle_key_press(SDL_Keycode keycode){
--- src/texture.h
+++ src/texture.h
@@ -2,17 +2,19 @@
 #define TEXTURE_H
 
 #include <stdint.h>
+#include "upng.h"
 
 typedef struct {
-  float u;
-  float v;
+    float u;
+    float v;
 } tex2_t;
 
 extern int texture_width;
 extern int texture_height;
 
-extern const uint8_t REDBRICK_TEXTURE[];
-
+extern upng_t* png_texture;
 extern uint32_t* mesh_texture;
+
+void load_png_texture_data(char* filename);
 
 #endif
