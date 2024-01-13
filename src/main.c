2a1c713444f689b89e3b250586f0509ac7a79bdb
Michal Zalobny <michal.zalobny@gmail.com>
2024-01-13 00:33:46 +0000

    Add new assets

--- src/main.c
+++ src/main.c
@@ -52,10 +52,10 @@ void setup(void){
 
   // Load the cube values in the mesh data structure
   // load_cube_mesh_data();
-  load_obj_file_data("./assets/cube.obj");
+  load_obj_file_data("./assets/f22.obj");
 
   // Load the texture information from an external PNG file
-  load_png_texture_data("./assets/cube.png");
+  load_png_texture_data("./assets/f22.png");
 }
 
 void handle_key_press(SDL_Keycode keycode){
